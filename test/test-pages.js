bba8390593f3c9cc1c0465271a719401b50113c7
Lucas Costi <lucascosti@users.noreply.github.com>
2020-09-14 17:38:35 +1000

    Update test-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -6,7 +6,7 @@ it('Page content test', (done) => {
   request.get('http://localhost:3000', function (err, res, body) {
       expect(res.statusCode).to.equal(200);
       expect(res).to.have.property('body');
-      expect(res).property('body').to.match(/Hello world, everything is awesome/);
+      expect(res).property('body').to.match(/Helcvcvxcvxclo world, everything is awesome/);
       done();
   });
 });
